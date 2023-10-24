53a2b7166d8c5036aebfc00af0991a5b6c14a862
captainrizzless <130604613+captainrizzless@users.noreply.github.com>
2023-10-24 17:09:14 -0500

    Split main() into other smaller functions, update function documentation, change some of the code to be more maintainable in the future.
    This is a pretty hefty commit, most of the file was modified in one way or another. Originally, I was thinking about rewriting bits to practice utilizing classes, but I'm not sure if it's necessary or would benefit the project in any way. This commit should allow the script to be maintained more easily.

--- src/package folder/main.py
+++ src/package folder/main.py
@@ -1,44 +1,12 @@
-# todo add doc strings and comments and stuff so future you and other people don't struggle to read your spaghetti code
 import scalestuff
 import modestuff
 import intervalstuff
 
-main_menu_prompt = ('Main Menu\n'
-                    'Program options:\n'
-                    '  (Show) Displays concepts.\n'
-                    '  (Hide) Removes concept from final review.\n'
-                    'Good to do for accidental searches.\n'
-                    '  (Print) Prints list that documents what the user\n'
-                    'has searched to review areas of focus for practice.\n'
-                    '  (Quit) Terminates program.\n'
-                    '\n'
-                    'Enter input: ')
 
-main_menu_options = ['Show', 'Hide', 'Print', 'Quit']
-
-table_of_contents_prompt = ('Concept options:\n'
-                            '  (Scales) Displays Scales.\n'
-                            '  (Modes) Displays Modes.\n'
-                            '  (Intervals) Displays basic Interval information.\n'
-                            '  (More Intervals) Displays information about Circle of Fifths/Fourths, Order of\n'
-                            'Sharps/Flats, etc.\n'
-                            '\n'
-                            'Enter concept: ')
-
-table_of_contents = ['Scales', 'Modes', 'Intervals', 'More Intervals']
-
-vocab = ['Show', 'Hide', 'Print', 'Quit', 'Scales', 'Modes', 'Intervals', 'More Intervals', 'Major', 'Major Pentatonic',
-         'Minor', 'Melodic Minor', 'Harmonic Minor', 'Minor Pentatonic', 'Ionian', 'Dorian', 'Phrygian', 'Lydian',
-         'Mixolydian', 'Aeolian', 'Locrian', 'Interval Basics', 'Chord Basics', 'Chord Progressions', 'Arpeggio Basics',
-         'Circles', 'Orders', 'Scale Degrees']  # todo Convert into dict for a 'help' command
-
-selected_topics = {}
-
-
-def validate(command):
+def invalidate(command):
     """
-
-    :param command: I.e. the user_input; warns user if unrecognized.
+    Displays text specific to why the command was invalid.
+    :param command: I.e. the user_input
     :return: None
     """
     if command == '':
@@ -60,7 +28,7 @@ def hide_check():
 
 def hide():
     """
-    Asks user what they would like to remove from hard-coded dictionary. Warns user if it wasn't found, otherwise it
+    Asks user what they would like to remove from standard dictionary. Warns user if key wasn't found, otherwise it
     deletes it.
     :return: None
     """
@@ -73,107 +41,220 @@ def hide():
 
 def print_check():
     """
-    Iterates through hard-coded dictionary and prints each element, otherwise it warns user if it's empty.
+    Iterates through standard dictionary and prints each key and corresponding value.
     :return: None
     """
     if not selected_topics:
         print('Nothin\' here.')
     else:
-        for key, value in selected_topics.items():
-            print(f'{key} --> {value}')
+        for topic, parent_topic in selected_topics.items():
+            print(f'{topic} --> {parent_topic}')
 
 
-def main():  # todo split this up into smaller functions
+def core_script_logic(command):
+    """
+    Executes relevant logic native to script (nothing required externally).
+    :param command: I.e. the user_input
+    :return: None
+    """
+    if command == 'Show':
+        for topic, info in table_of_contents.items():
+            print(display_topic_str.format(col1=topic, col2=info['descr']))
+    elif command == 'Hide':
+        hide_check()
+    elif command == 'Print':
+        print_check()
+
+
+def display_matches(command):
+    """
+    Displays all relevant topics related to the command that are found within table_of_contents.
+    :param command: I.e. the user_input
+    :return:None
+    """
+    for topic, info in table_of_contents.items():
+        if info['Parent Topic'] == command:
+            print(display_topic_str.format(col1=topic, col2=info['descr']))
+
+
+def fetch_topic_info(command):
+    """
+    Displays the relevant topic information from the imported modules and stores the search within a basic dictionary
+    using information from table_of_contents.
+    :param command: I.e. the user_input
+    :return: None
+    """
+    if command == 'Major':
+        print(scalestuff.major_scale_prompt)
+        selected_topics[command] = table_of_contents[command]['Parent Topic']
+    elif command == 'Major Pentatonic':
+        print(scalestuff.major_penta_scale_prompt)
+        selected_topics[command] = table_of_contents[command]['Parent Topic']
+    elif command == 'Minor':
+        print(scalestuff.minor_scale_prompt)
+        selected_topics[command] = table_of_contents[command]['Parent Topic']
+    elif command == 'Melodic Minor':
+        print(scalestuff.melminor_scale_prompt)
+        selected_topics[command] = table_of_contents[command]['Parent Topic']
+    elif command == 'Harmonic Minor':
+        print(scalestuff.harminor_scale_prompt)
+        selected_topics[command] = table_of_contents[command]['Parent Topic']
+    elif command == 'Minor Pentatonic':
+        print(scalestuff.minor_penta_scale_prompt)
+        selected_topics[command] = table_of_contents[command]['Parent Topic']
+    elif command == 'Scale Degrees':
+        scalestuff.formatted_scale_degrees()
+        selected_topics[command] = table_of_contents[command]['Parent Topic']
+    elif command == 'Ionian':
+        print(modestuff.ionian_mode_prompt)
+        selected_topics[command] = table_of_contents[command]['Parent Topic']
+    elif command == 'Dorian':
+        print(modestuff.dorian_mode_prompt)
+        selected_topics[command] = table_of_contents[command]['Parent Topic']
+    elif command == 'Phrygian':
+        print(modestuff.phrygian_mode_prompt)
+        selected_topics[command] = table_of_contents[command]['Parent Topic']
+    elif command == 'Lydian':
+        print(modestuff.lydian_mode_prompt)
+        selected_topics[command] = table_of_contents[command]['Parent Topic']
+    elif command == 'Mixolydian':
+        print(modestuff.mixolydian_mode_prompt)
+        selected_topics[command] = table_of_contents[command]['Parent Topic']
+    elif command == 'Aeolian':
+        print(modestuff.aeolian_mode_prompt)
+        selected_topics[command] = table_of_contents[command]['Parent Topic']
+    elif command == 'Locrian':
+        print(modestuff.locrian_mode_prompt)
+        selected_topics[command] = table_of_contents[command]['Parent Topic']
+    elif command == 'Interval Basics':
+        print(intervalstuff.interval_basics)
+        selected_topics[command] = table_of_contents[command]['Parent Topic']
+    elif command == 'Chord Basics':
+        print(intervalstuff.chord_basics)
+        selected_topics[command] = table_of_contents[command]['Parent Topic']
+    elif command == 'Chord Progressions':
+        print(intervalstuff.chord_progs)
+        selected_topics[command] = table_of_contents[command]['Parent Topic']
+    elif command == 'Arpeggio Basics':
+        print(intervalstuff.arpeggio_basics)
+        selected_topics[command] = table_of_contents[command]['Parent Topic']
+    elif command == 'Circles':
+        print(intervalstuff.circles_prompt)
+        selected_topics[command] = table_of_contents[command]['Parent Topic']
+    elif command == 'Orders':
+        print(intervalstuff.orders_prompt)
+        selected_topics[command] = table_of_contents[command]['Parent Topic']
+
+
+def gather_vocab():
+    for key in table_of_contents.keys():
+        vocab.append(key)
+    for key, item in table_of_contents.items():
+        if item['Parent Topic'] not in vocab:
+            vocab.append(item['Parent Topic'])
+    for core_command in core_script_commands:
+        vocab.append(core_command)
+
+
+def main():
     print("Welcome to the Rizzless Guitar Guide!\n")
     user_input = input(main_menu_prompt).strip().lower().title()
 
     while user_input != 'Quit':
-        if user_input == 'Show':
-            user_input = input(table_of_contents_prompt).strip().lower().title()
-            continue
-        elif user_input == 'Scales':
-            user_input = input(scalestuff.scales_prompt).strip().lower().title()
-            continue
-        elif user_input == 'Major':
-            print(scalestuff.major_scale_prompt)
-            selected_topics[user_input] = 'Scale'
-        elif user_input == 'Major Pentatonic':
-            print(scalestuff.major_penta_scale_prompt)
-            selected_topics[user_input] = 'Scale'
-        elif user_input == 'Minor':
-            print(scalestuff.minor_scale_prompt)
-            selected_topics[user_input] = 'Scale'
-        elif user_input == 'Melodic Minor':
-            print(scalestuff.melminor_scale_prompt)
-            selected_topics[user_input] = 'Scale'
-        elif user_input == 'Harmonic Minor':
-            print(scalestuff.harminor_scale_prompt)
-            selected_topics[user_input] = 'Scale'
-        elif user_input == 'Minor Pentatonic':
-            print(scalestuff.minor_penta_scale_prompt)
-            selected_topics[user_input] = 'Scale'
-        elif user_input == 'Scale Degrees':
-            scalestuff.formatted_scale_degrees()
-            selected_topics[user_input] = 'Scale'
-        elif user_input == 'Modes':
-            user_input = input(modestuff.modes_prompt).strip().lower().title()
-            continue
-        elif user_input == 'Ionian':
-            print(modestuff.ionian_mode_prompt)
-            selected_topics[user_input] = 'Mode'
-        elif user_input == 'Dorian':
-            print(modestuff.dorian_mode_prompt)
-            selected_topics[user_input] = 'Mode'
-        elif user_input == 'Phrygian':
-            print(modestuff.phrygian_mode_prompt)
-            selected_topics[user_input] = 'Mode'
-        elif user_input == 'Lydian':
-            print(modestuff.lydian_mode_prompt)
-            selected_topics[user_input] = 'Mode'
-        elif user_input == 'Mixolydian':
-            print(modestuff.mixolydian_mode_prompt)
-            selected_topics[user_input] = 'Mode'
-        elif user_input == 'Aeolian':
-            print(modestuff.aeolian_mode_prompt)
-            selected_topics[user_input] = 'Mode'
-        elif user_input == 'Locrian':
-            print(modestuff.locrian_mode_prompt)
-            selected_topics[user_input] = 'Mode'
-        elif user_input == 'Intervals':
-            user_input = input(intervalstuff.interval_menu).strip().lower().title()
-            continue
-        elif user_input == 'Interval Basics':
-            print(intervalstuff.interval_basics)
-            selected_topics[user_input] = 'Interval'
-        elif user_input == 'Chord Basics':
-            print(intervalstuff.chord_basics)
-            selected_topics[user_input] = 'Interval'
-        elif user_input == 'Chord Progressions':
-            print(intervalstuff.chord_progs)
-            selected_topics[user_input] = 'Interval'
-        elif user_input == 'Arpeggio Basics':
-            print(intervalstuff.arpeggio_basics)
-            selected_topics[user_input] = 'Interval'
-        elif user_input == 'More Intervals':
-            user_input = input(intervalstuff.more_intervals_menu).strip().lower().title()
-            continue
-        elif user_input == 'Circles':
-            print(intervalstuff.circles_prompt)
-            selected_topics[user_input] = 'More Intervals'
-        elif user_input == 'Orders':
-            print(intervalstuff.orders_prompt)
-            selected_topics[user_input] = 'More Intervals'
-        elif user_input == 'Hide':
-            hide_check()
-        elif user_input == 'Print':
-            print_check()
+        if user_input in core_script_commands:
+            core_script_logic(user_input)
+        elif user_input in ['Scales', 'Modes', 'Intervals', 'More Intervals']:
+            display_matches(user_input)
+        elif user_input in table_of_contents.keys():
+            fetch_topic_info(user_input)
         else:
-            validate(user_input)
+            invalidate(user_input)
 
         user_input = input(main_menu_prompt[26:]).strip().lower().title()
 
 
+main_menu_prompt = ('Main Menu\n'
+                    'Program options:\n'
+                    '  (Show) Displays concepts.\n'
+                    '  (Hide) Removes concept from final review.\n'
+                    'Good to do for accidental searches.\n'
+                    '  (Print) Prints list that documents what the user\n'
+                    'has searched to review areas of focus for practice.\n'
+                    '  (Quit) Terminates program.\n'
+                    '\n'
+                    'Enter input: ')
+
+main_menu_options = ['Show', 'Hide', 'Print', 'Quit']
+
+table_of_contents = {
+    'Major': {'descr': 'Shows Major scale info.',
+              'Parent Topic': 'Scales'
+              },
+    'Major Pentatonic': {'descr': 'Shows Major Pentatonic scale info.',
+                         'Parent Topic': 'Scales'
+                         },
+    'Minor': {'descr': 'Shows Minor scale info.',
+              'Parent Topic': 'Scales'
+              },
+    'Melodic Minor': {'descr': 'Shows Melodic Minor scale info.',
+                      'Parent Topic': 'Scales'
+                      },
+    'Harmonic Minor': {'descr': 'Shows Harmonic Minor scale info.',
+                       'Parent Topic': 'Scales'
+                       },
+    'Minor Pentatonic': {'descr': 'Shows Minor Pentatonic scale info.',
+                         'Parent Topic': 'Scales'
+                         },
+    'Scale Degrees': {'descr': 'Shows the scale degrees for all scales listed.',
+                      'Parent Topic': 'Scales'
+                      },
+    'Ionian': {'descr': 'Shows information for the Ionian mode.',
+               'Parent Topic': 'Modes'
+               },
+    'Dorian': {'descr': 'Shows information for the Dorian mode.',
+               'Parent Topic': 'Modes'
+               },
+    'Phrygian': {'descr': 'Shows information for the Phrygian mode.',
+                 'Parent Topic': 'Modes'
+                 },
+    'Lydian': {'descr': 'Shows information for the Lydian mode.',
+               'Parent Topic': 'Modes'
+               },
+    'Mixolydian': {'descr': 'Shows information for the Mixolydian mode.',
+                   'Parent Topic': 'Modes'
+                   },
+    'Aeolian': {'descr': 'Shows information for the Aeolian mode.',
+                'Parent Topic': 'Modes'
+                },
+    'Locrian': {'descr': 'Shows information for the Locrian mode.',
+                'Parent Topic': 'Modes'
+                },
+    'Interval Basics': {'descr': 'Shows information about interval names and their semitones.',
+                        'Parent Topic': 'Intervals'
+                        },
+    'Chord Basics': {'descr': 'Shows the basic fundamentals of how chords are made.',
+                     'Parent Topic': 'Intervals'
+                     },
+    'Chord Progressions': {'descr': 'Shows common chord progressions.',
+                           'Parent Topic': 'Intervals'
+                           },
+    'Arpeggio Basics': {'descr': 'Shows information about the basics of arpeggios.',
+                        'Parent Topic': 'Intervals'
+                        },
+    'Circles': {'descr': 'Shows information about the Circle of Fifths and the Circle of Fourths.',
+                'Parent Topic': 'More Intervals'
+                },
+    'Orders': {'descr': 'Shows information about the Orders of Sharps and the Order of Flats.',
+               'Parent Topic': 'More Intervals'
+               }
+}
+display_topic_str = '{col1:24}{col2:120}'
+core_script_commands = ['Show', 'Hide', 'Print']
+vocab = []
+selected_topics = {}
+
 if __name__ == '__main__':
+    gather_vocab()
     main()
 
 # todo create tests
