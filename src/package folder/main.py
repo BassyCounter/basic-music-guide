f20d9e3037e45b35825c1b7c0c1e123818fd127d
captainrizzless <130604613+captainrizzless@users.noreply.github.com>
2023-10-24 09:18:26 -0500

    Remove redundant code, add documentation to functions, and delete some empty print statements.
    The print statements are there specifically to split up the different parts of the output. However, I decided that it just makes the output feel more bulky than necesssary and I prefer it to be more compact to minimize the amount of scrolling up and down. Also, I switched to writing the commits imperatively so that I'm practicing following best practices and also it will make more sense when/if I need to revert to a previous commit.

--- src/package folder/main.py
+++ src/package folder/main.py
@@ -36,156 +36,137 @@ selected_topics = {}
 
 
 def validate(command):
+    """
+
+    :param command: I.e. the user_input; warns user if unrecognized.
+    :return: None
+    """
     if command == '':
-        print('Enter any command below to continue.')
-        return
+        print('Enter any command from below to continue.')
     elif command not in vocab:
         print('Unknown, please check for misspelling.')
-        return
 
 
 def hide_check():
+    """
+    Warns user if the container is empty and carries on, otherwise it calls hide().
+    :return: None
+    """
     if not selected_topics:
         print('No topics are available for review.')
-        return
     elif selected_topics:
         hide()
-        return
 
 
 def hide():
+    """
+    Asks user what they would like to remove from hard-coded dictionary. Warns user if it wasn't found, otherwise it
+    deletes it.
+    :return: None
+    """
     topic = input('Enter topic you wish to remove from topic review: ').strip().lower().title()
     if topic in selected_topics:
         del selected_topics[topic]
-        return
     else:
         print('Topic is either misspelled or has not been searched for.')
-        return
 
 
 def print_check():
+    """
+    Iterates through hard-coded dictionary and prints each element, otherwise it warns user if it's empty.
+    :return: None
+    """
     if not selected_topics:
         print('Nothin\' here.')
+    else:
+        for key, value in selected_topics.items():
+            print(f'{key} --> {value}')
 
 
-def topic_location():
-    for key, value in selected_topics.items():
-        print(f'{key} --> {value}')
-
-
-def main():
+def main():  # todo split this up into smaller functions
     print("Welcome to the Rizzless Guitar Guide!\n")
-    user_input = input(main_menu_prompt).strip().lower().title()  # Figure out how to delete all the starting print()s
+    user_input = input(main_menu_prompt).strip().lower().title()
 
     while user_input != 'Quit':
         if user_input == 'Show':
-            print()
             user_input = input(table_of_contents_prompt).strip().lower().title()
             continue
         elif user_input == 'Scales':
-            print()
             user_input = input(scalestuff.scales_prompt).strip().lower().title()
             continue
         elif user_input == 'Major':
-            print()
             print(scalestuff.major_scale_prompt)
             selected_topics[user_input] = 'Scale'
         elif user_input == 'Major Pentatonic':
-            print()
             print(scalestuff.major_penta_scale_prompt)
             selected_topics[user_input] = 'Scale'
         elif user_input == 'Minor':
-            print()
             print(scalestuff.minor_scale_prompt)
             selected_topics[user_input] = 'Scale'
         elif user_input == 'Melodic Minor':
-            print()
             print(scalestuff.melminor_scale_prompt)
             selected_topics[user_input] = 'Scale'
         elif user_input == 'Harmonic Minor':
-            print()
             print(scalestuff.harminor_scale_prompt)
             selected_topics[user_input] = 'Scale'
         elif user_input == 'Minor Pentatonic':
-            print()
             print(scalestuff.minor_penta_scale_prompt)
             selected_topics[user_input] = 'Scale'
         elif user_input == 'Scale Degrees':
-            print()
             scalestuff.formatted_scale_degrees()
             selected_topics[user_input] = 'Scale'
         elif user_input == 'Modes':
-            print()
             user_input = input(modestuff.modes_prompt).strip().lower().title()
             continue
         elif user_input == 'Ionian':
-            print()
             print(modestuff.ionian_mode_prompt)
             selected_topics[user_input] = 'Mode'
         elif user_input == 'Dorian':
-            print()
             print(modestuff.dorian_mode_prompt)
             selected_topics[user_input] = 'Mode'
         elif user_input == 'Phrygian':
-            print()
             print(modestuff.phrygian_mode_prompt)
             selected_topics[user_input] = 'Mode'
         elif user_input == 'Lydian':
-            print()
             print(modestuff.lydian_mode_prompt)
             selected_topics[user_input] = 'Mode'
         elif user_input == 'Mixolydian':
-            print()
             print(modestuff.mixolydian_mode_prompt)
             selected_topics[user_input] = 'Mode'
         elif user_input == 'Aeolian':
-            print()
             print(modestuff.aeolian_mode_prompt)
             selected_topics[user_input] = 'Mode'
         elif user_input == 'Locrian':
-            print()
             print(modestuff.locrian_mode_prompt)
             selected_topics[user_input] = 'Mode'
         elif user_input == 'Intervals':
-            print()
             user_input = input(intervalstuff.interval_menu).strip().lower().title()
             continue
         elif user_input == 'Interval Basics':
-            print()
             print(intervalstuff.interval_basics)
             selected_topics[user_input] = 'Interval'
         elif user_input == 'Chord Basics':
-            print()
             print(intervalstuff.chord_basics)
             selected_topics[user_input] = 'Interval'
         elif user_input == 'Chord Progressions':
-            print()
             print(intervalstuff.chord_progs)
             selected_topics[user_input] = 'Interval'
         elif user_input == 'Arpeggio Basics':
-            print()
             print(intervalstuff.arpeggio_basics)
             selected_topics[user_input] = 'Interval'
         elif user_input == 'More Intervals':
-            print()
             user_input = input(intervalstuff.more_intervals_menu).strip().lower().title()
             continue
         elif user_input == 'Circles':
-            print()
             print(intervalstuff.circles_prompt)
             selected_topics[user_input] = 'More Intervals'
         elif user_input == 'Orders':
-            print()
             print(intervalstuff.orders_prompt)
             selected_topics[user_input] = 'More Intervals'
         elif user_input == 'Hide':
-            print()
             hide_check()
-            print()
         elif user_input == 'Print':
-            print()
             print_check()
-            topic_location()
         else:
             validate(user_input)
 
